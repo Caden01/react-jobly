86d6bcc29826697b813dc9404e8e9c32e4f8e744
Caden Carlson <caden@Cadens-MacBook-Pro.local>
2024-05-08 13:50:01 -0700

    Create profile form so users can make changes to their profile

---
+++ my-app/src/profiles/ProfileForm.js
@@ -0,0 +1,96 @@
+import React, { useState, useContext } from "react";
+import JoblyApi from "../api/api";
+import UserContext from "../auth/UserContext";
+import { useContext } from "react";
+
+function ProfileForm() {
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+  const [formData, setFormData] = useState({
+    username: currentUser.username,
+    password: "",
+    firstName: currentUser.firstName,
+    lastName: currentUser.lastName,
+    email: currentUser.email,
+  });
+  async function handleSubmit(evt) {
+    evt.preventDefault();
+
+    let profileData = {
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      email: formData.email,
+      password: formData.password,
+    };
+    let username = formData.username;
+    let updatedUser;
+
+    try {
+      updatedUser = await JoblyApi.saveProfile(username, profileData);
+    } catch (errors) {
+      console.errors("Updating profile failed", errors);
+      return;
+    }
+
+    setFormData((f) => ({ ...f, password: "" }));
+    setCurrentUser(updatedUser);
+  }
+
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    setFormData((f) => ({ ...f, [name]: value }));
+  }
+
+  return (
+    <div>
+      <div>
+        <h2>Profile</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Username</label>
+            <p>{formData.username}</p>
+          </div>
+          <div>
+            <label>First Name</label>
+            <input
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label>Last Name</label>
+            <input
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label>Email</label>
+            <input
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button onSubmit={handleSubmit}>Save Changes</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default SignupForm;
